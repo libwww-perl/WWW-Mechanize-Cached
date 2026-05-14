cac5ead88190bad8a304c8dd3be587dc87de1b08
Olaf Alders <olaf@wundersolutions.com>
2026-05-14 16:26:12 +0000

    increment $VERSION after 2.00 release

--- lib/WWW/Mechanize/Cached.pm
+++ lib/WWW/Mechanize/Cached.pm
@@ -3,7 +3,7 @@ package WWW::Mechanize::Cached;
 use Moo 1.004005;
 use warnings FATAL => 'all';
 
-our $VERSION = '1.57';
+our $VERSION = '2.01';
 
 use Carp                         qw( carp croak );
 use Module::Runtime              qw( use_module );
